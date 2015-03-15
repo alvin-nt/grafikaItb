8498906f328c619beb149a11e25a8b1b5daf7651
Muhammad Reza Irvanda <muzavan@gmail.com>
2015-03-15 21:21:10 +0700

    Color Pick Tinggal Copy colorPicker()

--- demo_rectangle.cpp
+++ demo_rectangle.cpp
@@ -21,7 +21,7 @@ using std::endl;
 // handles ctrl-c
 // temporarily not used
 void INTHandler(int dummy);
-
+Color colorPicker();
 void cleanup();
 
 Keyboard *keyboard;
@@ -41,70 +41,8 @@ int main()
 	keyboard = new Keyboard();
 	screen = Screen::instance(); // singleton
 //	screen->setMode(GRAPHICS);
-
-	bool exit = false;
-
-	// the main program loop
-	
-	// initialize the rectangle
-	Rectangle *rect = new Rectangle(50, 200, Color::BLUE,
-									50, 300, Color::BLUE,
-									250, 300,Color::BLUE,
-									250, 200,Color::BLUE,10.0f);
-									
-	Rectangle *rect1 = new Rectangle(300, 200, Color::RED,
-									300, 300, Color::RED,
-									500, 300, Color::RED,
-									500, 200, Color::RED,10.0f);
-									
-	Rectangle *rect2 = new Rectangle(550, 200, Color::GREEN,
-									550, 300, Color::GREEN,
-									750, 300, Color::GREEN,
-									750, 200, Color::GREEN,10.0f);
-									
-	Rectangle *outline = new Rectangle(40, 190, Color::WHITE,
-									40, 310, Color::WHITE,
-									260, 310, Color::WHITE,
-									260, 190, Color::WHITE,10.0f);
-	rect->setFillColor(Color::BLUE);
-	rect1->setFillColor(Color::RED);
-	rect2->setFillColor(Color::GREEN);			
-	int movHorizontal = 5, movVertical = 5;
-	
-	while(!exit) {
-		int key = keyboard->getPressedKeyCode();
-
-		if(key == Keyboard::NO_INPUT) {
-			key = 0;
-		} else {
-			switch(key) {
-			case KEY_BACKSPACE:
-				exit = true;
-				break;
-			case KEY_LEFT:
-				outline->move(-250,0);
-				break;
-			case KEY_RIGHT:
-				outline->move(250,0);
-				break;
-			case KEY_UP:
-				break;
-			case KEY_DOWN:
-				break;
-			}
-		}
-
-		screen->drawBackground();
-		screen->draw(rect,true);
-		screen->draw(rect2,true);
-		screen->draw(rect1,true);
-		screen->draw(outline);
-		screen->update();
-
-		// sleep
-		usleep(5000);
-	}
-	delete rect;
+	Color color = colorPicker();
+	printf("Selesai");
 	cleanup();
 	
 	return 0;
@@ -120,4 +58,85 @@ void cleanup() {
 	screen->setMode(TEXT);
 	Screen::destroy();
 	delete keyboard;
+}
+
+Color colorPicker(){
+	bool exit = false;
+	int pil=1;
+	// the main program loop
+	
+	// initialize the rectangle
+	Rectangle *rect1 = new Rectangle(50, 200, Color::BLUE,
+									50, 300, Color::BLUE,
+									250, 300,Color::BLUE,
+									250, 200,Color::BLUE,10.0f);
+									
+	Rectangle *rect2 = new Rectangle(300, 200, Color::RED,
+									300, 300, Color::RED,
+									500, 300, Color::RED,
+									500, 200, Color::RED,10.0f);
+									
+	Rectangle *rect3 = new Rectangle(550, 200, Color::GREEN,
+									550, 300, Color::GREEN,
+									750, 300, Color::GREEN,
+									750, 200, Color::GREEN,10.0f);
+									
+	Rectangle *outline = new Rectangle(40, 190, Color::WHITE,
+									40, 310, Color::WHITE,
+									260, 310, Color::WHITE,
+									260, 190, Color::WHITE,10.0f);
+	rect1->setFillColor(Color::BLUE);
+	rect2->setFillColor(Color::RED);
+	rect3->setFillColor(Color::GREEN);		
+	
+	while(!exit) {
+		int key = keyboard->getPressedKeyCode();
+
+		if(key == Keyboard::NO_INPUT) {
+			key = 0;
+		} else {
+			switch(key) {
+			case KEY_BACKSPACE:
+				exit = true;
+				break;
+			case KEY_LEFT:
+				outline->move(-250,0);
+				if(pil!=1)
+					pil--;
+				break;
+			case KEY_RIGHT:
+				outline->move(250,0);
+				if(pil!=3)
+					pil++;
+				break;
+			case KEY_UP:
+				break;
+			case KEY_DOWN:
+				break;
+			}
+		}
+
+		screen->drawBackground();
+		screen->draw(rect1,true);
+		screen->draw(rect3,true);
+		screen->draw(rect2,true);
+		screen->draw(outline);
+		screen->update();
+		printf("Hampir Selesai");
+		// sleep
+		usleep(500);
+	}
+	switch(pil){
+		case 1 :
+			return rect1->getFillColor();
+		break;
+		case 2 :
+			return rect2->getFillColor();
+		break;
+		case 3 :
+			return rect3->getFillColor();
+		break;
+	}
+	
+	
 }
